5fe62b9213c9f41371ffc624a53aa7a01f79f7a3
Prakhar <prakharsaxena3416@gmail.com>
2025-04-12 03:48:19 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Home from './components/Home.jsx';
-import About from "./Components/About.jsx";
-import Certificate from "./Components/Certificate.jsx";
-import Showcase from "./Components/Showcase.jsx";
-import Contact from "./Components/Contact.jsx";
+import About from "./components/About.jsx";
+import Certificate from "./components/Certificate.jsx";
+import Showcase from "./components/Showcase.jsx";
+import Contact from "./components/Contact.jsx";
 import "./App.css";
 
 const App = () => {
